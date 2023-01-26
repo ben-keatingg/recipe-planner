febd441cf389b00f4efbcd653ffe7c43ab693203
ben-keatingg <ben.keating@thebodycoach.com>
2023-01-26 20:02:13 +0000

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from "react"
+import Logo from "../logo/Logo"
+import styles from './Header.module.css'
+
+interface Props {
+  hideSubHeader?: boolean
+}
+
+const Header: React.FC<Props> = ({ hideSubHeader }) => {
+
+  return (
+    <Fragment>
+      <Logo className={styles['logo']}/>
+      <div className={styles['header-container']}>
+        <h1 className={styles['header-title']}>Meal Planner</h1>
+        {!hideSubHeader && (
+          <p className={styles['header-text']}>Use our handy planner to easily plan your meals for the week</p>
+        )}
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/header/Header'
 
 export default function Home() {
   return (
@@ -9,8 +10,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Hello</h1>
+      <main className="content">
+        <Header />
       </main>
       </>
   )
