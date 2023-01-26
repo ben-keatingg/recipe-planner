0ae6d47f82b365f590b16e3e0ac5a75c04998dbe
ben-keatingg <ben.keating@thebodycoach.com>
2023-01-26 23:47:58 +0000

    added happy path test for home page

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,57 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react"
+import axios from "axios"
+import { addDays } from "date-fns"
+import { v4 as uuid } from 'uuid'
+import Home from "."
+import { dateToDay } from "../lib/dates"
+
+const mockPush = jest.fn()
+jest.mock('next/router', () => {
+  return {
+    useRouter: jest.fn(() => ({ push: mockPush }))
+  }
+})
+
+jest.mock('axios', () => {
+  return {
+    post: jest.fn()
+  }
+})
+
+beforeEach(() => {
+  mockPush.mockReset()
+})
+
+describe('Home Page', () => {
+
+  it('Allows you to select a day, 2 health goals and save', async () => {
+    const userId = uuid()
+    render(<Home planFromServer={null}  userId={userId}/>)
+    const dateToSelect = addDays(new Date(), 3)
+    const dateTestId = `day-box-${dateToDay(dateToSelect).toISOString().slice(0,10)}`
+
+    fireEvent.click(screen.getByTestId(dateTestId))
+    
+    expect(screen.getByTestId(dateTestId).className).toContain('day-container--selected')
+
+    fireEvent.click(screen.getByLabelText('Brain Health'))
+    fireEvent.click(screen.getByLabelText('Mental Wellbeing'))
+
+    fireEvent.click(screen.getByText('Start Planning'))
+
+    const expectedPlan = {
+      "healthGoals": ["brain-health", "mental-wellbeing"],
+      "plannedDays": [],
+      "startDate": "2023-01-29T00:00:00.000Z",
+       "userId": userId
+    }
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith('/api/plan', expectedPlan)
+    })
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith('/plan')
+    })
+    
+  })
+})
